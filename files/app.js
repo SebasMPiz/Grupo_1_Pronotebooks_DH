a3b50a6b3f15ac484d12ccdabbea5519d751baf2
Sebastian Pizarro <le.sebiex@gmail.com>
2022-05-19 11:13:47 -0300

    Prueba

--- files/app.js
+++ files/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.listen(3030, () => console.log("El servidor esta corriendo en puerto 3030"))
 
-app.use(express.static(path.join(__dirname, "./public")))
+app.use(express.static("/public"))
 
 app.get ("/", (req,res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"))})
