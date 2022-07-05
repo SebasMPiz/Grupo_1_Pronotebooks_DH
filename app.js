1f8aff12f400d0b632f3715dcd6afcf31555becd
SebasMPiz <le.sebiex@gmail.com>
2022-07-05 14:04:45 -0300

    funcional users controller y se actualiza lista e usuarios

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(session( {secret: "Nuestro mensaje secreto"}));
+app.use(session( {secret: "Nuestro mensaje secreto", resave: false, saveUninitialized:false}));
 app.use(cookieParser())
 app.use(methodOverride('_method'))
 
