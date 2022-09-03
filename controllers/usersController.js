2a3cd9eed023a517252409bac456f565e9e05696
Lucas <montobybianchi@gmail.com>
2022-09-02 23:28:52 -0300

    02-09-22

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -33,50 +33,100 @@ const usersController = {
 				
             })},
 
-			register: (req,res)=>{
+	register: (req,res)=>{
 				users.findAll({
-					
-				})
-					.then(users => {
-						res.render('users/register', {users}) 
-						// res.json(brand.length)
+		})
+			.then(users => {
+				res.render('users/register', {users}) 
+				// res.json(brand.length)
 						
-					})},
-
-	// register: (req,res)=>{
-	// 	res.render('users/register');
-			
-	// 	},
+			})},
 	
-	// login: (req,res)=>{
-	// 	users.findAll({
-
-	// 	})
-	// 		.then(users => {
-	// 			res.render('users/login')
+	login: (req,res)=>{
+		users.findAll({
+		})
+			.then(users => {
+				res.render('users/login', {users})
 				
-	// 		})},
+			})},
+	
+	loginProcess: (req, res) => {
+			let userToLogin = User.findByField('email', req.body.email, {users});
+			if(userToLogin) {
+			let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
+				if (isOkThePassword) {
+					delete userToLogin.password;
+					req.session.userLogged = userToLogin;
+			
+					if(req.body.remember_user) {
+						res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
+						}
+			
+					return res.redirect('profile');
+					} 
+					return res.render('users/login', {
+					errors: {
+					email: {
+					msg: 'Las credenciales son inválidas'
+						}
+					}
+					});
+					}
+					return res.render('users/login', {
+					errors: {
+					email: {
+					msg: 'No se encuentra este email en nuestra base de datos'
+							}
+						}
+					});
+				},
 
+	logout: (req, res) => {
+		res.clearCookie('userEmail');
+			req.session.destroy();
+			return res.redirect('/');
+				},
 
-
-
-
-
-
-
+	store: (req, res) => {
+		console.log(req.file)
+			let newUser = {
+			id: users[users.length - 1].Id + 1,
+			...req.body,
+			password: bcryptjs.hashSync(req.body.password, 10),
+			image: req.file ? req.file.filename : 'defaultuser.jfif' }
+						
+			users.push(newUser)
+			// fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "))
+			res.redirect("/users/list")	
+				},
+			
+		profile: (req, res) => {
+			return res.render('users/myProfile', {
+			users: req.session.userLogged
+				});
+				},
+				
+	
+	
+	
+	
+	
+	
+				//
     // list: (req,res)=>{
 	// 	console.log(req.cookies.userEmail)
     //     res.render('users/list', { users });
     // },
+
 	// login: (req, res) => {
 	// 	if(res.locals.isLogged) {
 	// 		return res.render("users/profile")
 	// 	}
 	// 	return res.render('users/login');
 	// },
+
 	// loginProcess: (req, res) => {
 	// 	let userToLogin = User.findByField('email', req.body.email);
-
 	// 	if(userToLogin) {
 	// 		let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
 	// 		if (isOkThePassword) {
@@ -105,10 +155,11 @@ const usersController = {
 	// 		}
 	// 	});
 	// },
+
     // register: (req,res)=>{
     //     res.render('users/register');
-
     // },
+
 	// store: (req, res) => {
 	// 	console.log(req.file)
 	// 	let newUser = {
@@ -121,22 +172,25 @@ const usersController = {
 	// 		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "))
 	// 		res.redirect("/users/list")	
 	// },
+
 	// profile: (req, res) => {
 	// 	return res.render('users/myProfile', {
 	// 		users: req.session.userLogged
 	// 	});
 	// },
+
 	// detail: (req, res) => {
 	// 	let idUser = req.params.id;
 	// 	let user = users.find(product => product.id == idUser)
-		
 	// 	res.render("users/myProfile", { users:user })
 	// },
+
     // editUser: (req, res) => {
 	// 	let id = req.params.id
 	// 	let editUser = users.find(user => user.id == id)
 	// 	res.render("users/editUser", { editUser })
     // },
+
     // update: (req, res) => {
 	// 	let id = req.params.id 
 	// 	let editUser = users.find(user => user.id == id) 
@@ -146,7 +200,6 @@ const usersController = {
 	// 		password: bcrypt.hashSync(req.body.password, 10),
 	// 		image: req.file ? req.file.filename : 'defaultuser.jfif'
 	// 	}; 
-
 	// 	let newUsers = users.map(user => {
 													 
 	// 		if (user.id === editUser.id) {
@@ -157,6 +210,7 @@ const usersController = {
 	// 	fs.writeFileSync(usersFilePath, JSON.stringify(newUsers, null, ' '));
 	// 	res.redirect("/users/list");
 	// },
+
 	// destroy: (req, res) => {
 	// 	let id = req.params.id  
 	// 	let finalUsers = users.filter(user => user.id != id) 
@@ -164,6 +218,7 @@ const usersController = {
   	// 	fs.writeFileSync(usersFilePath, JSON.stringify(finalUsers, null, ' '));
 	// 	res.redirect("/users/list")
 	// },
+
 	// logout: (req, res) => {
 	// 	res.clearCookie('userEmail');
 	// 	req.session.destroy();
--- routes/users.js
+++ routes/users.js
@@ -11,14 +11,14 @@ const authMiddleware  = require('../middlewares/authMiddleware');
 /*** GET ALL USERS ***/
 router.get("/", usersController.list)
 
-// /*** LOGIN ***/ 
-// router.get('/login', guestMiddleware, usersController.login);
-// // router.post('/login', usersController.loginProcess);
-// router.get('/logout/', usersController.logout)
+/*** LOGIN ***/ 
+router.get('/login', guestMiddleware, usersController.login);
+router.post('/login', usersController.loginProcess);
+router.get('/logout/', usersController.logout)
 
 /*** CREATE ONE USER ***/ 
 router.get ("/register", usersController.register)
-// router.post ('/register', uploadUserFile.single('image'), usersController.store);
+router.post ('/register', uploadUserFile.single('image'), usersController.store);
 
 // /*** DETAIL MY PROFILE ***/ 
 // router.get("/profile", authMiddleware, usersController.profile)
