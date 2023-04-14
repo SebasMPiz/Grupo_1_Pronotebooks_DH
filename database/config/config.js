d52f7f1e08b716945443b263815e9f266bad5994
LUCAS\monto <lucasmontoby@gmail.com>
2023-04-14 00:22:28 -0300

    cambio nombre de base de datos

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "",
-    "database": "pronotebooks",
+    "database": "pronotebook",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port": "3306",
